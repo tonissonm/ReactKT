3544e9b45e24d74e309bca8a723a46b81c08d128
Matis Tonisson <matistonisson@gmail.com>
2024-11-01 13:13:58 +0200

    Ül5

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -1,4 +1,5 @@
 const MealItem = (props) => {
+    let currency = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(props.meal.price);
     return (
         <li className="meal-item">
             <article className="meal-item article">
@@ -9,8 +10,8 @@ const MealItem = (props) => {
                 />
                 <div className="meal-info">
                     <h3 className="meal-item-name">{props.meal.name}</h3>
-                    <p className="meal-item-price">{props.meal.price}</p>
-                    <p className="meal-item-description">{props.meal.description}</p>
+                    <p className="meal-item-price">{currency}</p>
+                    <p className="meal-item-description"> {props.meal.description}</p>
                 </div>
                 <div className="meal-item-actions">
                     <button>Add to Cart</button>
@@ -20,5 +21,5 @@ const MealItem = (props) => {
         
     )
 }
-
+/* className="button"*/ 
 export default MealItem
